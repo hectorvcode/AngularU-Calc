920be9d857ade48c0509b2ee655e48775e34cf6b
hectorvcode <hectorfabiovasquez@gmail.com>
2022-01-29 17:14:37 -0500

    Created calculator app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-calc';
+  operandoA = 0;
+  operandoB = 0;
+  resultado = 0;
+
+  sumar(){
+    this.resultado = this.operandoA + this.operandoB;
+    return this.resultado;
+  }
 }
